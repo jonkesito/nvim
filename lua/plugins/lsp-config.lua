2bf9cf387fb2f354ea3335878f2517888c6b6139
Jonkeiber Casique <118020934+jonkesito@users.noreply.github.com>
2024-03-29 19:42:40 -0400

    Agreagando AI coding y actualizando tailwind

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,102 +1,108 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "cssls", "html", "tsserver", "tailwindcss", "volar", "sqlls", "jsonls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "cssls", "html", "tsserver", "tailwindcss", "volar", "sqlls", "jsonls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.volar.setup({
-				filetypes = {
-					"typescript",
-					"javascript",
-					"javascriptreact",
-					"typescriptreact",
-					"vue",
-					"json",
-					"html",
-					"css",
-					"scss",
-				},
-        init_options = {
-            typescript = {
-            tsdk = "/Users/jonkibook/.nvm/versions/node/v21.7.1/lib/node_modules/typescript/lib"
-          }
+      lspconfig.volar.setup({
+        filetypes = {
+          "typescript",
+          "javascript",
+          "javascriptreact",
+          "typescriptreact",
+          "vue",
+          "json",
+          "html",
+          "css",
+          "scss",
         },
-				capabilities = capabilities,
-			})
+        init_options = {
+          typescript = {
+            tsdk = "/Users/jonkibook/.nvm/versions/node/v21.7.1/lib/node_modules/typescript/lib",
+          },
+        },
+        capabilities = capabilities,
+      })
 
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-			})
+      local on_attach = function(client, bufnr)
+        -- other stuff --
+        require("tailwindcss-colors").buf_attach(bufnr)
+      end
 
-			lspconfig.sqlls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
 
-			local configs = require("lspconfig/configs")
-			capabilities.textDocument.completion.completionItem.snippetSupport = true
+      lspconfig.sqlls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.emmet_ls.setup({
-				-- on_attach = on_attach,
-				capabilities = capabilities,
-				filetypes = {
-					"css",
-					"eruby",
-					"html",
-					"javascript",
-					"javascriptreact",
-					"less",
-					"sass",
-					"scss",
-					"svelte",
-					"pug",
-					"typescriptreact",
-					"vue",
-				},
-				init_options = {
-					html = {
-						options = {
-							-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-							["bem.enabled"] = true,
-						},
-					},
-				},
-			})
-		end,
-	},
+      local configs = require("lspconfig/configs")
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      lspconfig.emmet_ls.setup({
+        -- on_attach = on_attach,
+        capabilities = capabilities,
+        filetypes = {
+          "css",
+          "eruby",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "less",
+          "sass",
+          "scss",
+          "svelte",
+          "pug",
+          "typescriptreact",
+          "vue",
+        },
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      })
+    end,
+  },
 }
--- lua/plugins/tailwind-color.lua
+++ lua/plugins/tailwind-color.lua
@@ -1,14 +1,6 @@
 return {
-	{
-		"roobert/tailwindcss-colorizer-cmp.nvim",
-		config = function()
-			require("tailwindcss-colorizer-cmp").setup({
-				color_square_width = 2,
-			})
-
-			require("cmp").config.formatting = {
-				format = require("tailwindcss-colorizer-cmp").formatter,
-			}
-		end,
-	},
+  "themaxmarchuk/tailwindcss-colors.nvim",
+  config = function ()
+    require("tailwindcss-colors").setup()
+  end
 }
