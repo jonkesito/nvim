6b4593730b275e670696831761ae6939a477d9c3
Jonkeiber Casique <118020934+jonkesito@users.noreply.github.com>
2024-04-03 18:38:06 -0400

    instalando tmux e integrandolo con el

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,29 +1,32 @@
 return {
-  {
-    "antonyz89/electron-vue.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-  },
-  {
-    "numToStr/Comment.nvim",
-    keys = { "gcc" },
-    config = function()
-      require("Comment").setup()
-    end,
-  },
-  {
-    "windwp/nvim-autopairs",
-  },
-  {
-    "windwp/nvim-ts-autotag",
-  },
+	{
+		"antonyz89/electron-vue.nvim",
+		dependencies = { "rktjmp/lush.nvim" },
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		keys = { "gcc" },
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+	},
+	{
+		"windwp/nvim-ts-autotag",
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+	},
 }
